9e5d53fb59d4144df21a0280e0adfb1f667f4753
giorgos_shittas <georgeshittas55@gmail.com>
2023-11-07 14:48:48 +0200

    TEST

--- app/src/main/java/com/example/lostpets/FirstFragment.java
+++ app/src/main/java/com/example/lostpets/FirstFragment.java
@@ -10,7 +10,7 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.lostpets.databinding.FragmentFirstBinding;
-
+//Heloooooooo JULIOOOO
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
